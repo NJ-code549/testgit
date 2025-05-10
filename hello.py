4a12650986f8809f4ebc765cb514ff5196981d67
NJ-code549 <lakshanjeyakumar@yahoo.co.uk>
2025-05-10 10:33:22 +0300

    added secrest text

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
-class namegreeter:
-    def __init__(self,name):
-        self.name=name
 
-    def greet(self):
-        print(f"{self.name} ,how can i help you today jacob?")
 
-ng=namegreeter("sam")
-ng.greet()
+my_bio="My name is %9898 _]\'; niranjan"
+print(my_bio.upper())
+print(my_bio.lower())
+print(my_bio.strip().replace("9898","7894"))
+print(my_bio.__contains__(str))
